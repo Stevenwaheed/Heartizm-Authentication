a6053fe6ddd7f968d7e00c08d3f93c991bdd503a
Steven W. Saad <83607748+Stevenwaheed@users.noreply.github.com>
2023-04-25 21:24:38 +0200

    Update ECG.py

--- ECG.py
+++ ECG.py
@@ -1058,5 +1058,5 @@ def authenticate_result():
         return jsonify({'Result':'Authenticated'})
 
 
-# if __name__ == '__main__':
-app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
